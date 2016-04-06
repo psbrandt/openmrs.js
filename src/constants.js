36c9a60410445a3c7a00c39fe35a9aad1ad33dea
Pascal Brandt <psbrandt@gmail.com>
2016-04-05 19:26:38 -0700

    ☝️ Bump version.

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 export const OMRSJS_CLASS_NAME = 'OpenMRS';
-export const OMRSJS_VERSION = '0.0.1';
+export const OMRSJS_VERSION = '0.0.2';
 
 export const OMRSJS_DEFAULT_PROTOCOL = 'http';
 export const OMRSJS_OPENAPI_SPEC_PATH_SUFFIX = 'module/webservices/rest/swagger.json';
